35dd2b50a74c79df3cf30f5c0b068541dc346c86
solodev <basboyzamaniey@gmail.com>
2026-04-09 13:15:19 +0100

    premitive types

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -2,8 +2,15 @@ fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
     // let a = ???
 
+    //let a = [5; 100]; // 100 elements, all = 5
+
+    // let a = [0; 100];
+
+
+    let a: [i32; 100] = std::array::from_fn(|i| i as i32);
+
     if a.len() >= 100 {
-        println!("Wow, that's a big array!");
+        println!("Wow, that's a big array! {:?}", a);
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
         panic!("Array not big enough, more elements needed");
